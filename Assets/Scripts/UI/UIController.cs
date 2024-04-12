6185d5e663a41c6677e8fbfbac4fec3365743ad5
Attila <attilatoth03@outlook.com>
2024-04-12 19:47:52 +1000

    Finished Pause Menu

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,9 +1,51 @@
+using System;
+
+using Unity.Collections;
+
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
+	[SerializeField] private GameObject pauseMenu;
+	[SerializeField] private InputActionReference pauseMenuReference;
+	
+	[SerializeField, ReadOnly] private bool isPaused;
+	
 	public void ChangeScene(string _sceneToChangeTo) => SceneManager.LoadSceneAsync(_sceneToChangeTo, LoadSceneMode.Single);
 
 	public void Quit() => Application.Quit();
+
+	private void Awake() => pauseMenuReference.action.performed += Paused;
+
+	private void Start()
+	{
+		if(pauseMenu == null) return;
+		if(pauseMenuReference == null) return;
+		
+		isPaused = false;
+	}
+
+	private void Update()
+	{
+		if(isPaused)
+		{
+			pauseMenu.gameObject.SetActive(true);
+			Time.timeScale = 0;
+		}
+		else
+		{
+			Time.timeScale = 1;
+			pauseMenu.gameObject.SetActive(false);
+		}
+	}
+
+	private void Paused(InputAction.CallbackContext obj) => isPaused = !isPaused;
+
+	public void Resume() => isPaused = false;
+
+	private void OnEnable() => pauseMenuReference.action.Enable();
+
+	private void OnDisable() => pauseMenuReference.action.Disable();
 }
