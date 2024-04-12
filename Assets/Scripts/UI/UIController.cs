11a65ce2f7a9e3f3dc72b227e9030cee3e6f881b
Attila <attilatoth03@outlook.com>
2024-04-12 16:45:59 +1000

    Added New Scenes + Added UI Controller

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+	public void ChangeScene(string _sceneToChangeTo)
+	{
+		SceneManager.LoadSceneAsync(_sceneToChangeTo, LoadSceneMode.Single);
+	}
+
+	public void Quit()
+	{
+			Application.Quit();
+	}
+}
